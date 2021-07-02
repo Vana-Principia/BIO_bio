f9f3f7e0ef719f82d202c0c5eb63cc71778d500f
ADX-S17 <antony.desmeaux@gmail.com>
2021-07-02 20:34:56 +0200

    This is the first step.

---
+++ main.cpp
@@ -0,0 +1,8 @@
+// This software aims to create an expert 2.0
+// Please feel free to improve this solution, we'll need as much willing soul as possible. For the greater good.
+
+#include <string>
+
+std::string main(int argc, char* argv) {
+	return "No result yet";
+}
