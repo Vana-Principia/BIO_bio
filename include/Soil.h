d9119f01099f1c9856ca82aaa55919eaa0b8c3a4
ADX-S17 <antony.desmeaux@gmail.com>
2021-07-05 15:01:52 +0200

    Soil added

---
+++ include/Soil.h
@@ -0,0 +1,22 @@
+/*
+ * Soil.h
+ *
+ *  Created on: 5 juil. 2021
+ *      Author: antony
+ */
+
+#ifndef INCLUDE_SOIL_H_
+#define INCLUDE_SOIL_H_
+
+class Soil {
+public:
+	Soil();
+	~Soil();
+
+private:
+
+};
+
+
+
+#endif /* INCLUDE_SOIL_H_ */
---
+++ src/Soil.cpp
@@ -0,0 +1,25 @@
+/*
+ * Position.cpp
+ *
+ *  Created on: 3 juil. 2021
+ *      Author: antony
+ */
+
+#include "../include/Position.h"
+
+Position::Position() {
+	X = 0;
+	Y = 0;
+	Z = 0;
+}
+
+Position::Position(int coordX, int coordY, int coordZ) {
+	X = coordX;
+	Y = coordY;
+	Z = coordZ;
+}
+
+Position::~Position() {
+
+}
+
